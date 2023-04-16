06e1a99be4c8771b030c83719930ded7389a12c0
Bhushan-Wanjari <118363675+Bhushan-Wanjari@users.noreply.github.com>
2023-04-16 20:37:43 +0530

    scoreboard api done

--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -6,15 +6,81 @@ const bcrypt = require("bcryptjs");
 router.post("/", async function (req, res, next) {
   const { email } = req.body;
 
-  User.findOne({ email: email })
-    .then((docs) => {
-      console.log("Result :", docs);
-      return res.json(docs);
-    })
-    .catch((err) => {
-      console.log(err);
+  let users;
+  let data=[];
+  let allData;
+  let count=0,avgtime=0,avgatt=0,mintime=1000000000,minatt=1000000000,time,attempt,rank, level;
+  let level1,level2,level3,level4,level5;
+  try {
+    users = await User.find();
+    for (let a = 0; a < users.length; a++) {
+      if(email==users[a].email){
+        level=users[a].level;
+        time =users[a].game1[2] +users[a].game2[2] +users[a].game3[2] +users[a].game4[2] +users[a].game5[2];
+        attempt= users[a].game1[1] +users[a].game2[1] +users[a].game3[1] +users[a].game4[1] +users[a].game5[1];
+        level1=users[a].game1;
+        level2=users[a].game2;
+        level3=users[a].game3;
+        level4=users[a].game4;
+        level5=users[a].game5;
+      }
+      if (users[a].level == 6) {
+        const totalTime =users[a].game1[2] +users[a].game2[2] +users[a].game3[2] +users[a].game4[2] +users[a].game5[2];
+        const totalAtt= users[a].game1[1] +users[a].game2[1] +users[a].game3[1] +users[a].game4[1] +users[a].game5[1];
+        avgtime=avgtime+totalTime;
+        avgatt=avgatt+totalAtt;
+        count=count+1;
+        if(totalAtt<minatt){
+          minatt=totalAtt;
+        }
+        if(totalTime<mintime){
+          mintime=totalTime;
+        }
+        let dumm = {
+          rank: a,
+          username: users[a].name,
+          email: users[a].email,
+          time: totalTime,
+        };
+        data.push(dumm);
+      }
+    }
+    avgatt=Math.ceil(avgatt/count);
+    avgtime=Math.ceil(avgtime/count);
+    data.sort((a, b) => {
+      return a.time - b.time;
     });
-
+    for (let a = 0; a < data.length; a++) {
+      data[a].rank = a + 1;
+      if(data[a].email==email){
+        rank=a+1; 
+        break;
+      }
+    }
+    allData={
+      email: email,
+      rank: rank,
+      time: time,
+      attempt: attempt,
+      mintime: mintime,
+      minattempt: minatt,
+      avgtime: avgtime,
+      avgattempt: avgatt,
+      level: level,
+      game1: level1,
+      game2: level2,
+      game3: level3,
+      game4: level4,
+      game5: level5
+    }
+    return res.status(200).json({ allData });
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ message: "No Users Found" });
+  }
+  if (!users) {
+    return res.status(404).json({ message: "No Users Found" });
+  }
 });
 
 module.exports = router;
